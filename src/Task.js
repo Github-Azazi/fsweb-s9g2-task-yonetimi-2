b21b9449330a81a3288f0cbbd60b719604dbb4b7
Github-Azazi <you@example.com>
2023-12-11 21:22:45 +0300

    S9G2 Done

--- src/Task.js
+++ src/Task.js
@@ -4,16 +4,16 @@ import { formatDistanceToNow, differenceInDays } from "date-fns";
 const Task = ({ taskObj, onComplete }) => {
   const diff = differenceInDays(new Date(taskObj.deadline), new Date());
   const mathAbs = Math.abs(diff);
-  const bgColor = diff < 3 ? "bg-red-100" : "bg-blue-100"
-  
+  const bgColor = diff < 3 ? "bg-red-100" : "bg-blue-100";
+
   return (
     <div className="task">
       <h3 className="font-bold text-purple-950 text-xl hover:text-purple-700">
         {taskObj.title}
-        </h3>
+      </h3>
       <div className="deadline">
         <span className={bgColor}>
-          Son teslim: 
+          Son teslim:
           {mathAbs} gün
           {diff > 0 ? " sonra" : " önce"}
         </span>
@@ -22,14 +22,26 @@ const Task = ({ taskObj, onComplete }) => {
       <div className="flex justify-between">
         <div className="flex flex-row">
           {taskObj.people.map((p) => (
-            <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full mx-2" key={p}>
-              <div className="flex flex-col">
-                <div>
-                <svg class="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+            <div
+              className="relative overflow-hidden mx-2 "
+              key={p}
+            >
+              <div className="flex flex-col items-center">
+                <div className="bg-gray-100 rounded-full p-2 ">
+                  <svg
+                    class="w-8 h-8 text-gray-400 "
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
                 </div>
-                <div>
-                  {p}
-                </div>
+                <div>{p}</div>
               </div>
             </div>
           ))}
